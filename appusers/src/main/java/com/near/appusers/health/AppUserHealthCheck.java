61fb18bfd8292dc0fd78d0dae9dc283c08a98a46
zwkero <zwkero@gmail.com>
2015-01-21 21:13:22 -0800

    Update AppUserHealthCheck.java

--- appusers/src/main/java/com/near/appusers/health/AppUserHealthCheck.java
+++ appusers/src/main/java/com/near/appusers/health/AppUserHealthCheck.java
@@ -2,6 +2,12 @@ package com.near.appusers.health;
 
 import com.codahale.metrics.health.HealthCheck;
 
+/**
+ * This Class to test general Health of the server.. 
+ * 
+ * @author zwkero
+ *
+ */
 public class AppUserHealthCheck extends HealthCheck {
   @Override
   protected Result check() throws Exception {  
