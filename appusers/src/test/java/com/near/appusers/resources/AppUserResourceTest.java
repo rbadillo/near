cdb8e2b3b710e063b0d1b535330f93e676867e8b
zwkero <zwkero@gmail.com>
2015-01-25 13:07:37 -0800

    Update AppUserResourceTest.java

--- appusers/src/test/java/com/near/appusers/resources/AppUserResourceTest.java
+++ appusers/src/test/java/com/near/appusers/resources/AppUserResourceTest.java
@@ -1,23 +1,19 @@
 package com.near.appusers.resources;
-
 import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import com.near.appusers.api.AppUser;
+import com.google.common.base.Optional;
 
 import org.junit.Test;
-import com.google.common.base.Optional;
 
 public class AppUserResourceTest {
   @Test
   public void testCreateAppUser() {
     ArrayList<AppUser> AppUsers = new ArrayList<AppUser>();
     String expectedName = "Expected";
-    Integer expectedAge = 99;
-    
-    AppUser expectedAppUser = new AppUser(0, expectedName, expectedAge);
+    AppUser expectedAppUser = new AppUser("0", expectedName);
     AppUserResource AppUserResource = new AppUserResource(AppUsers);
-    
-    AppUserResource.createAppUser(Optional.of(expectedName), Optional.of(expectedAge));
+    AppUserResource.createAppUser(Optional.of(expectedName));
     
     AppUser createdAppUser = AppUsers.get(0);
     System.out.print(createdAppUser.equals(expectedAppUser));
